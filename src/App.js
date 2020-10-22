5999f155ef2faa908d85c05c931a7691da281f6b
CJHydraGenZ <cahyasaga321@gmail.com>
2020-10-22 12:29:56 +0800

    commit all

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Home from './pages/Home'
 import City from './pages/City'
 import RestaurantDetail from './pages/RestaurantDetail'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Navbar />
-        <Route path="/" exact component={Home}></Route>
+        <Route path="/kp_web_kuliner" exact component={Home}></Route>
         <Route path="/city/:city_id" component={City}></Route>
         <Route path="/restaurant/:restaurant_id" component={RestaurantDetail}></Route>
         <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-    <Link className="navbar-brand" to="/">FoodParadise</Link>
+    <Link className="navbar-brand" to="/kp_web_kuliner">FoodParadise</Link>
   </nav>
 )
 
